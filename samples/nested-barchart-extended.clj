9b8012abde5cfb4e64596181cb097d5eae29df61
Jan Aerts <jan.aerts@gmail.com>
2012-05-15 00:00:35 +0200

    Added axes

--- samples/nested-barchart-extended.clj
+++ samples/nested-barchart-extended.clj
@@ -8,7 +8,8 @@ body { background-color:white;}
 .total { fill:grey; opacity: 0.2; stroke:none;}
 .male { fill:blue; stroke:none;}
 .female { fill:pink; stroke:none;}
-text { stroke:grey; font-family:serif;}
+text { fill:grey; font-family:serif; stroke:none}
+line { stroke: black;}
 ")
 
 (let [data [{:label "A" :m 5 :f 13} {:label "B" :m 8 :f 4} {:label "C" :m 7 :f 11} {:label "D" :m 11 :f 9}]
@@ -16,8 +17,7 @@ text { stroke:grey; font-family:serif;}
 	  indexed-data (map-indexed vector data)]
   [:svg
 	[:style {:type "text/css"} (str "<![CDATA[" css "]]>")]
-	[:line {:x1 0 :x2 800 :y1 350 :y2 350}]
-	[:g.chart {:transform (translate [0 20])}
+	[:g.chart {:transform (translate [50 50])}
 		[:g.total
 			(unify indexed-data (fn [[idx data-point]]
 				[:rect {:x (* idx 25) :y 0 :width 20 :height (+ (s (:m data-point)) (s (:f data-point)))}]))]
@@ -27,6 +27,14 @@ text { stroke:grey; font-family:serif;}
 		[:g.female
 			(unify indexed-data (fn [[idx data-point]]
 				[:rect {:x (+ (* idx 25) 10) :y 0 :width 10 :height (s (:f data-point))}]))]]
-	[:g.labels
+	[:g.labels {:transform (translate [50 25])}
 		(unify indexed-data (fn [[idx data-point]]
-			[:text {:x (* idx 25) :y 18} (:label data-point)]))]])
+			[:text {:x (* idx 25) :y 18} (:label data-point)]))]
+	[:g.axes {:transform (translate [30 48])}
+		[:line {:x1 18 :y1 0 :x2 118 :y2 0}]
+		[:line {:x1 18 :y1 0 :x2 18 :y2 450}]
+		(unify [100 200 300 400] (fn [tick]
+			[:line {:x1 12 :y1 tick :x2 18 :y2 tick}]))
+		(unify [100 200 300 400] (fn [tick]
+			[:text {:x -20 :y tick} (str tick)]))]
+])
