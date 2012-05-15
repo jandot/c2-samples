2d933a8eb368a26e7d0cc10b4c8b475fbf5fa740
Jan Aerts <jan.aerts@gmail.com>
2012-05-15 09:29:50 +0200

    Flipped barchart upside-down

--- samples/nested-barchart-extended.clj
+++ samples/nested-barchart-extended.clj
@@ -20,21 +20,21 @@ line { stroke: black;}
 	[:g.chart {:transform (translate [50 50])}
 		[:g.total
 			(unify indexed-data (fn [[idx data-point]]
-				[:rect {:x (* idx 25) :y 0 :width 20 :height (+ (s (:m data-point)) (s (:f data-point)))}]))]
+				[:rect {:x (* idx 25) :y (- 300 (+ (s (:m data-point)) (s (:f data-point)))) :width 20 :height (+ (s (:m data-point)) (s (:f data-point)))}]))]
 		[:g.male
 			(unify indexed-data (fn [[idx data-point]]
-				[:rect {:x (* idx 25) :y 0 :width 10 :height (s (:m data-point))}]))]
+				[:rect {:x (* idx 25) :y (- 300 (s (:m data-point))) :width 10 :height (s (:m data-point))}]))]
 		[:g.female
 			(unify indexed-data (fn [[idx data-point]]
-				[:rect {:x (+ (* idx 25) 10) :y 0 :width 10 :height (s (:f data-point))}]))]]
-	[:g.labels {:transform (translate [50 25])}
+				[:rect {:x (+ (* idx 25) 10) :y (- 300 (s (:f data-point))) :width 10 :height (s (:f data-point))}]))]]
+	[:g.labels {:transform (translate [50 350])}
 		(unify indexed-data (fn [[idx data-point]]
 			[:text {:x (* idx 25) :y 18} (:label data-point)]))]
-	[:g.axes {:transform (translate [30 48])}
+	[:g.axes {:transform (translate [30 350])}
 		[:line {:x1 18 :y1 0 :x2 118 :y2 0}]
-		[:line {:x1 18 :y1 0 :x2 18 :y2 450}]
-		(unify [100 200 300 400] (fn [tick]
-			[:line {:x1 12 :y1 tick :x2 18 :y2 tick}]))
-		(unify [100 200 300 400] (fn [tick]
-			[:text {:x -20 :y tick} (str tick)]))]
+		[:line {:x1 18 :y1 (- 350) :x2 18 :y2 0}]
+		(unify [100 200 300] (fn [tick]
+			[:line {:x1 12 :y1 (- tick) :x2 18 :y2 (- tick)}]))
+		(unify [100 200 300] (fn [tick]
+			[:text {:x -20 :y (- tick)} (str tick)]))]
 ])
