28ab8678424eebaae49d030dad19085078c6cacc
Jan Aerts <jan.aerts@gmail.com>
2012-05-25 10:52:47 +0200

    Split hilbert functions into create-structure, encode and decode. Decode returns x,y coords, not yet index/position

--- samples/hilbert.clj
+++ samples/hilbert.clj
@@ -1,11 +1,12 @@
 (ns hilbert)
 
-(defn encode [n]
+(defn create-structure [n]
 	(if (= n 0)
-		[[0],[0]]
-		(let [coords (encode (- n 1))
-			  x-s (first coords)
-			  y-s (second coords)
+		[[0,0]]
+		(let [coords (create-structure (- n 1))
+			  x-s (map #(first %) coords)
+			  y-s (map #(second %) coords)
+			  ; positions (range 0 (apply * (repeat n 4)))
 			  x-first (map #(*(- % 0.5) 0.5) y-s)
 			  x-second (map #(*(- % 0.5) 0.5) x-s)
 			  x-third (map #(*(+ % 0.5) 0.5) x-s)
@@ -14,6 +15,26 @@
 			  y-second (map #(*(+ % 0.5) 0.5) y-s)
 			  y-third (map #(*(+ % 0.5) 0.5) y-s)
 			  y-fourth (map #(*(- -0.5 %) 0.5) x-s)]
-			[(flatten (lazy-cat [x-first x-second x-third x-fourth])) (flatten (lazy-cat [y-first y-second y-third y-fourth]))])))
+			
+			(partition 2 (interleave
+				(flatten (lazy-cat [x-first x-second x-third x-fourth])) (flatten (lazy-cat [y-first y-second y-third y-fourth])))))))
 
-(println (encode 9))
+(defn encode [pos, n] "Returns x and y coordinate for a given position and order. 0-encoded."
+	(let [data (create-structure n)]
+		(if (or (> pos (count data)) (> 0 pos))
+			nil
+			(nth data pos))))
+
+(defn decode [x, y, n] "Returns the position along the curve for a given x and y coordinate and order. 0-encoded"
+	(filter #(= x (first %)) (filter #(= y (second %)) (create-structure n))))
+
+(println (create-structure 0))
+(println (create-structure 1))
+(println (create-structure 2))
+(println (encode 2 1))
+(println (encode 2 2))
+(println (encode 5 1))
+(println (encode 0 1))
+(println (encode -1 1))
+(println (decode -0.25 0.25 1))
+(println (decode -0.125 0.375 2))
